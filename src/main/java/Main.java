894e7aa116092c4eab75dbbfefae7a21eec0ac8c
Maxim Subbotin <subbotinmax01@gmail.com>
2024-12-17 17:15:12 +0800

    убрал 1-ую строку в Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Util.getConnection();
+
         UserDao userDao = new UserDaoJDBCImpl();
 
         userDao.createUsersTable();
